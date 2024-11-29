de0280c9a692aba353d8b342a80931114ad42f7a
thanadol1159 <87893349+thanadol1159@users.noreply.github.com>
2024-11-29 23:38:55 +0700

    allow capstone

--- backend/source/backend/settings.py
+++ backend/source/backend/settings.py
@@ -87,11 +87,11 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://your-frontend-domain.com",
-# ]
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://cp24nk1.sit.kmutt.ac.th/",
+]
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
