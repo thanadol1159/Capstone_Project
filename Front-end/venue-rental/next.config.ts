a839b0f7e5104b384387e9d0ba85a2d4aab825ae
Menoa <m_tanachart@hotmail.com>
2024-11-30 22:55:37 +0700

    fix config

--- Front-end/venue-rental/next.config.ts
+++ Front-end/venue-rental/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/nk1",
+  // basePath: "/nk1",
   output: "standalone",
   reactStrictMode: true,
   // swcMinify: true,
