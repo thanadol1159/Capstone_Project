d9a23122ae952f3d380cb7040d671d422d7e5c43
Menoa <m_tanachart@hotmail.com>
2024-11-30 15:17:46 +0700

    fix next config

--- Front-end/venue-rental/next.config.ts
+++ Front-end/venue-rental/next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/nk1",
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/nk1/api/:path*",
-  //       destination: "http://django:8080/api/:path*",
-  //     },
-  //   ];
-  // },
+  basePath: "/nk1",
+  reactStrictMode: true,
+  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
