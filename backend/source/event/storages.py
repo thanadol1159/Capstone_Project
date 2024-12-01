2016e2e5ba34704e098affab69a5703c244b196c
Menoa <m_tanachart@hotmail.com>
2024-12-01 14:10:51 +0700

    fix base path back

--- backend/source/event/storages.py
+++ backend/source/event/storages.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = 'media'
-    custom_domain = f"{settings.MINIO_ENDPOINT}/{bucket_name}"
+    custom_domain = f"{settings.MINIO_ENDPOINT}/nk1/{bucket_name}"
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = 'static'
-    custom_domain = f"{settings.MINIO_ENDPOINT}/{bucket_name}"
+    custom_domain = f"{settings.MINIO_ENDPOINT}/nk1/{bucket_name}"
