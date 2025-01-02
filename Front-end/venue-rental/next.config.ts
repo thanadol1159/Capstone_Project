c552fb0128412af36791cf132295ca9ea6dfb9ec
Tanachart Bunyarittiwan <50836433+Lahirerr@users.noreply.github.com>
2025-01-02 15:19:55 +0700

    fix base path

--- Front-end/venue-rental/next.config.ts
+++ Front-end/venue-rental/next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: "/nk1",
+  basePath: "/nk1",
   output: "standalone",
   reactStrictMode: true,
   // swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  basePath: "http://capstone24.sit.kmutt.ac.th/nk1/",
   assetPrefix: "http://capstone24.sit.kmutt.ac.th/nk1/",
   // redirects: async () => [
   //   {
