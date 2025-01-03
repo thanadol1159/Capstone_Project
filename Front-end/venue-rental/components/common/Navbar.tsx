48bacc0b454f74dda71e8a7f5b24441d9449adfd
Tanachart Bunyarittiwan <50836433+Lahirerr@users.noreply.github.com>
2025-01-03 15:03:58 +0700

    fix navbar

--- Front-end/venue-rental/components/common/Navbar.tsx
+++ Front-end/venue-rental/components/common/Navbar.tsx
@@ -21,7 +21,7 @@ const Navigation = () => {
 
   const navItems = accessToken
     ? [
-        { label: "Venue Rental", href: "/" },
+        { label: "Venue Rental", href: "/nk1" },
         {
           label: "Management",
           href: "#",
