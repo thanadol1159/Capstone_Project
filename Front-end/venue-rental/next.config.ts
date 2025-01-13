1de4d7c9cbc23f119242e5503607ef6fe07606c0
Tanachart Bunyarittiwan <50836433+Lahirerr@users.noreply.github.com>
2025-01-13 13:45:49 +0700

    change config

--- Front-end/venue-rental/next.config.ts
+++ Front-end/venue-rental/next.config.ts
@@ -4,19 +4,10 @@ const nextConfig: NextConfig = {
   // basePath: "/nk1",
   output: "standalone",
   reactStrictMode: true,
-  // swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   assetPrefix: "http://capstone24.sit.kmutt.ac.th/nk1/",
-  // redirects: async () => [
-  //   {
-  //     source: "/",
-  //     destination: "/venue-rental",
-  //     permanent: false,
-  //   },
-  // ],
-  // output: "standalone",
 };
 
 export default nextConfig;
