97e00153e5403d275c54a00722dd244cc049a5d1
Tanachart Bunyarittiwan <50836433+Lahirerr@users.noreply.github.com>
2025-01-20 18:41:12 +0700

    fix config

--- Front-end/venue-rental/next.config.ts
+++ Front-end/venue-rental/next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/nk1",
+  // basePath: "/nk1",
   output: "standalone",
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  assetPrefix: "https://capstone24.sit.kmutt.ac.th/nk1",
+  // assetPrefix: "https://capstone24.sit.kmutt.ac.th/nk1/",
 };
 
 export default nextConfig;
