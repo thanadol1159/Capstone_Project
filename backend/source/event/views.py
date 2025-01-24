5bb0c856b2283c4c26a78a6069c0c8f61c88684e
Tanachart Bunyarittiwan <50836433+Lahirerr@users.noreply.github.com>
2025-01-24 19:15:46 +0700

    put user in try

--- backend/source/event/views.py
+++ backend/source/event/views.py
@@ -81,9 +81,9 @@ class AccountViewSet(viewsets.ModelViewSet):
                 {"error": "password are required length more than 8 charactor"},
                 status=status.HTTP_400_BAD_REQUEST
             )
-            User.objects.create_user(username=username, password=password)
-        try:
             
+        try:
+            User.objects.create_user(username=username, password=password)
             Account.objects.create(username=username, password=make_password(password))
             return Response(
                 {"message": "Account and User created successfully!"},
