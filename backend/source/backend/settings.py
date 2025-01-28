54f6f37f0b1d37897f9568080ea286e3102fcfb3
thanadol1159 <87893349+thanadol1159@users.noreply.github.com>
2025-01-29 02:20:01 +0700

    add status to resquest  venue

--- backend/source/backend/settings.py
+++ backend/source/backend/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
