a856952df336c80f6285d34b1caefca02989ac13
thanadol1159 <87893349+thanadol1159@users.noreply.github.com>
2025-03-02 19:39:23 +0700

    import contentifle

--- backend/source/event/serializers.py
+++ backend/source/event/serializers.py
@@ -3,6 +3,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+import base64
+from django.core.files.base import ContentFile
 from .models import (
     Role,
     # Account,
