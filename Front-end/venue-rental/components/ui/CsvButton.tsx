5f3a09f1506977892b910d27c20a42a02ae1eb50
Tanachart Bunyarittiwan <50836433+Lahirerr@users.noreply.github.com>
2025-03-31 02:14:48 +0700

    add nk1

--- Front-end/venue-rental/components/ui/CsvButton.tsx
+++ Front-end/venue-rental/components/ui/CsvButton.tsx
@@ -12,10 +12,10 @@ export default function ExportCSVButton() {
 
     try {
       const response = await axios.get(
-        "https://capstone24.sit.kmutt.ac.th/nk1/export-venues/"
+        "https://capstone24.sit.kmutt.ac.th/nk1/nk1/export-venues/"
       );
       if (response.data.file_url) {
-        setFileUrl(`https://capstone24.sit.kmutt.ac.th/nk1/${response.data.file_url}`);
+        setFileUrl(`https://capstone24.sit.kmutt.ac.th/nk1/nk1/${response.data.file_url}`);
       }
     } catch (error) {
       console.error("Error exporting CSV:", error);
